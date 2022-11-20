74e95955ffc5213a7deda2f5bf83d266d6971e18
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-11-20 20:55:44 +0600

    recursion problem

--- recQs.cpp
+++ recQs.cpp
@@ -10,10 +10,56 @@ bool isSorted(int arr[], int n){
     return (arr[0] < arr[1] && restArray);
 }
 
+void dec(int n){
+    if(n == 0){
+        return;
+    }
+
+    cout << n << endl;
+    dec(n - 1);
+}
+
+void inc(int n){
+    if(n == 0){
+        return;
+    }
+
+    inc(n - 1);
+    cout << n << endl;
+}
+
+// int lastOcc(int arr, int n, int i, int key) {
+//     if(i == n){
+//         return -1;
+//     }
+
+//     lastOcc(arr, n, i++, key);
+//     if(arr[i] == key){
+//         return i;
+//     }
+// }
+
+void reverse(char arr[], int n, int i){
+
+
+    if(i == n){
+        return;
+    }
+    reverse(arr, n, i+1);
+    cout << arr[i];
+}
+
 int main(){
 
-    int arr[5] = {1, 2, 3, 4, 5};
-    cout << isSorted(arr, 5);
+    // int arr[3] = {1, 2, 1};
+    // cout << isSorted(arr, 3);
+
+    // inc(5);
+
+    // cout << lastOcc(arr, 3, 0, 2);
+
+    char arr[] = "hello";
+    reverse(arr, 5, 0);
 
     return 0;
 }
--- recurson.cpp
+++ recurson.cpp
@@ -1,11 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void reverse(string str){
+
+    if(str.length() == 0)
+        return;
+
+    char ch = str[0];
+    reverse(str.substr(1));
+    cout << ch;
+}
+
+string removeDup(string str){
+
+    if(str.length() == 0)
+        return "";
+
+    char ch = str[0];
+    string ros = removeDup(str.substr(1));
+
+    if(ch == ros[0]){
+        return ros;
+    }else{
+        return ch + ros;
+    }
+}
+
+string removex(string str){
+
+    if(str.length() == 0)
+        return "";
+
+    char ch = str[0];
+    string ros = removex(str.substr(1));
+
+    if(ch == 'x'){
+        return ros + ch;
+    }
+    
+    return ch + ros;
+    
+}
+
 int main(){
 
     // 1. Factorial of n
     // 2. num power of pow
     // 3. Fibonacci of n
+
+    // reverse("Riyad");
+    // cout << removeDup("aaabbcdee");
+    cout << removex("xxxaaaxxxbbcdxxee");
 
     return 0;
 }
